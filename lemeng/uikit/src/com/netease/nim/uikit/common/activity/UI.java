11f5152254d37a4c3dc33fed9fe4639192d203f2
shangshuaibo <15911065676@163.com>
2017-06-08 16:07:48 +0800

    网易云信根布局隐藏底部虚拟按键，设置全屏

--- lemeng/uikit/src/com/netease/nim/uikit/common/activity/UI.java
+++ lemeng/uikit/src/com/netease/nim/uikit/common/activity/UI.java
@@ -55,8 +55,46 @@ public abstract class UI extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             // 设置状态栏颜色
             window.setStatusBarColor(Color.parseColor("#F6D42C"));
-
         }
+
+        //设置虚拟按键变成三个小点
+        final Window window = getWindow();
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        window.setAttributes(params);
+
+        /**
+         * 虚拟键盘弹出来监听
+         */
+        window.getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                setHideVirtualKey(window);
+            }
+        });
+    }
+
+
+    /**
+     * 隐藏虚拟键盘
+     * @param window
+     */
+    public void setHideVirtualKey(Window window){
+        //保持布局状态
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE|
+                //布局位于状态栏下方
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
+                //全屏
+                View.SYSTEM_UI_FLAG_FULLSCREEN|
+                //隐藏导航栏
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        if (Build.VERSION.SDK_INT>=19){
+            uiOptions |= 0x00001000;
+        }else{
+            uiOptions |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        }
+        window.getDecorView().setSystemUiVisibility(uiOptions);
     }
 
     @Override
