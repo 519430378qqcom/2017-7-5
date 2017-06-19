dabb03c7e848f71a8c159c6612d10a79dabf0f1b
jhlws <614322516@qq.com>
2017-06-19 20:23:43 +0800

    测试

--- lemeng/app/src/main/java/com/lvshandian/lemeng/UrlBuilder.java
+++ lemeng/app/src/main/java/com/lvshandian/lemeng/UrlBuilder.java
@@ -22,7 +22,7 @@ public class UrlBuilder {
     public static final String SERVER_URL = "http://60.205.114.36:80/";// 服务器网址公网
     public static final String CHARGE_SERVER_URL = "http://60.205.114.36:80/admin/";// 充值端口
     public static final String SERVER_URL_8080 = "http://60.205.114.36:8080/";// 服务器网址公网
-    public static final String CHARGE_SERVER_URL_8080 = "http://60.205.114.36:8080/admin/";// 充值 端口
+    public static final String CHARGE_SERVER_URL_8080 = "http://60.205.114.36:8080/admin/";// 充值端口
 
     //乐檬本地服務器
 //    public static final String SERVER_URL = "http://10.11.1.119:8080/";// 服务器网址公网
