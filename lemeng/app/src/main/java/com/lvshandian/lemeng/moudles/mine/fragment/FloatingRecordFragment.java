7817f21cdf71ce100c91d110944e7d054cbed63f
Administrator <dgsimle@sina.com>
2017-06-24 19:08:31 +0800

    个人中心，盈亏记录，时间选择

--- lemeng/app/src/main/java/com/lvshandian/lemeng/moudles/mine/fragment/FloatingRecordFragment.java
+++ lemeng/app/src/main/java/com/lvshandian/lemeng/moudles/mine/fragment/FloatingRecordFragment.java
@@ -1,16 +1,14 @@
 package com.lvshandian.lemeng.moudles.mine.fragment;
 
-import android.graphics.drawable.ColorDrawable;
-import android.support.v4.content.ContextCompat;
+import android.app.Activity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.Display;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.NumberPicker;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.lvshandian.lemeng.R;
@@ -23,16 +21,15 @@ import com.lvshandian.lemeng.utils.LogUtils;
 import com.lvshandian.lemeng.widget.refresh.SwipeRefresh;
 import com.lvshandian.lemeng.widget.refresh.SwipeRefreshLayout;
 import com.lvshandian.lemeng.widget.view.EmptyRecyclerView;
+import com.lvshandian.lemeng.widget.view.LWheelDialog;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -41,7 +38,8 @@ import butterknife.Bind;
 /**
  * 盈亏记录
  */
-public class FloatingRecordFragment extends BaseFragment implements View.OnClickListener, SwipeRefresh.OnRefreshListener, SwipeRefreshLayout.OnPullUpRefreshListener {
+public class FloatingRecordFragment extends BaseFragment implements View.OnClickListener,
+        SwipeRefresh.OnRefreshListener, SwipeRefreshLayout.OnPullUpRefreshListener, LWheelDialog.ConfirmListener {
     @Bind(R.id.ll_empty)
     LinearLayout ll_empty;
     @Bind(R.id.refreshLayout)
@@ -93,30 +91,49 @@ public class FloatingRecordFragment extends BaseFragment implements View.OnClick
         recordAdapter = new FloatingRecordAdapter(mContext, floatingRecorcList);
         recyclerView.setAdapter(recordAdapter);
         recyclerView.setEmptyView(ll_empty);
-        initDate();
-        selecteFloating();
+        starttime = initDate(new Date(System.currentTimeMillis()));
+        selecteFloating(appUser.getId(), page, starttime, endtime);
     }
 
-    private void initDate() {
+    /**
+     * 时间格式化
+     *
+     * @param data
+     * @return
+     */
+    private String initDate(Date data) {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM");
-        Date date = new Date(System.currentTimeMillis());
-        String str = df.format(date);
-
+        String str = df.format(data) + "-01 00:00:00";
+        return str;
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.floating_calendar:
-                showPopupWindow();
+                showDatePicker();
                 break;
         }
 
     }
 
-    private void selecteFloating() {
+    /**
+     * 获取盈亏记录
+     *
+     * @param userId
+     * @param pagenum
+     * @param starttime
+     * @param endtime   可不传，写null即可
+     */
+    private void selecteFloating(String userId, int pagenum, String starttime, String endtime) {
+        String url = null;
         page = isRefresh ? 1 : ++page;
-        String url = UrlBuilder.GAME_BASE + String.format(UrlBuilder.PROFIT_AND_LOSS_RECORD, "100018", String.valueOf(page), starttime, endtime);
+        if (endtime == null || "".equals(endtime)) {
+            url = UrlBuilder.GAME_BASE + String.format(UrlBuilder.PROFIT_AND_LOSS_RECORD, userId, 10, String.valueOf(page), starttime);
+        } else {
+            url = UrlBuilder.GAME_BASE + String.format(UrlBuilder.PROFIT_AND_LOSS_RECORD, userId, 10, String.valueOf(page), starttime) + "&endtime=" + endtime;
+        }
+        LogUtils.e(url);
         OkHttpUtils.get().url(url).build().execute(new StringCallback() {
             @Override
             public void onError(com.squareup.okhttp.Request request, Exception e) {
@@ -153,205 +170,18 @@ public class FloatingRecordFragment extends BaseFragment implements View.OnClick
         });
     }
 
-    int SetDate = 5;
-    private NumberPicker np3;
-    private int day = 9;
-    private int month = 5;
-    private int year = 2000;
-
-    private int currentSet_year = 2000;
-    private int currentSet_month = 5;
-    private int currentSet_day = 18;
-
-    public void showPopupWindow() {
-        Calendar cal = Calendar.getInstance();
-        day = cal.get(Calendar.DATE);
-        month = cal.get(Calendar.MONTH) + 1;
-        year = cal.get(Calendar.YEAR);
-        currentSet_year = year;
-        currentSet_month = month;
-        currentSet_day = day;
-
-        View contentView = getActivity().getLayoutInflater().inflate(R.layout.float_record_fragment_calendar, null);
-        TextView textCancel = (TextView) contentView.findViewById(R.id.fragment_float_record_calendar_dismiss);
-        TextView textConfirm = (TextView) contentView.findViewById(R.id.fragment_float_record_calendar_confirm);
-        NumberPicker np1 = (NumberPicker) contentView.findViewById(R.id.fragment_floating_record_numberPicker1);
-        NumberPicker np2 = (NumberPicker) contentView.findViewById(R.id.fragment_floating_record_numberPicker2);
-        np3 = (NumberPicker) contentView.findViewById(R.id.fragment_floating_record_numberPicker3);
-
-        np1.setMaxValue(3000);
-        np1.setMinValue(1);
-        np1.setValue(year);
-        setNumberPickerDividerColor(np1);
-
-
-        np2.setMaxValue(12);
-        np2.setMinValue(1);
-        np2.setValue(month);
-        setNumberPickerDividerColor(np1);
-        setNumberPickerDividerColor(np2);
-
-        np1.setFormatter(new NumberPicker.Formatter() {
-            @Override
-            public String format(int value) {
-                SetDate = 1;
-                String tmpStr = String.valueOf(value);
-                if (value < 10) {
-                    tmpStr = "0" + tmpStr;
-                }
-                return tmpStr;
-            }
-        });
-
-        np2.setFormatter(new NumberPicker.Formatter() {
-            @Override
-            public String format(int value) {
-                String tmpStr = String.valueOf(value);
-                if (value < 10) {
-                    tmpStr = "0" + tmpStr;
-                }
-                return tmpStr;
-            }
-        });
-
-        if (leapyear(year) == true) {
-            SetDate = 29;
-        } else {
-            SetDate = 28;
-        }
-
-        np3.setMaxValue(SetDate);
-        np3.setMinValue(1);
-        np3.setValue(day);
-        setNumberPickerDividerColor(np3);
-
-        np1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
-                //    Toast.makeText(MainActivity.this,"oldVal"+newVal,Toast.LENGTH_LONG).show();
-
-                if (leapyear(newVal) == true) {
-                    SetDate = 29;
-                } else {
-                    SetDate = 28;
-                }
-
-                //  SetDate=newVal;
-                currentSet_year = newVal;
-
-
-                np3.setMaxValue(SetDate);
-                np3.setMinValue(1);
-                np3.setValue(day);
-
-
-                setNumberPickerDividerColor(np3);
-
-                np3.setFormatter(new NumberPicker.Formatter() {
-                    @Override
-                    public String format(int value) {
-                        String tmpStr = String.valueOf(value);
-                        if (value < 10) {
-                            tmpStr = "0" + tmpStr;
-                        }
-                        return tmpStr;
-                    }
-                });
-
-                np3.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-                    @Override
-                    public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
-                        //      Toast.makeText(MainActivity.this,"oldVal"+newVal,Toast.LENGTH_LONG).show();
-                        currentSet_day = newVal;
-                    }
-                });
-
-            }
-        });
-
-        np2.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
-                //     Toast.makeText(MainActivity.this,"oldVal"+newVal,Toast.LENGTH_LONG).show();
-                currentSet_month = newVal;
-            }
-        });
-
-
-        final PopupWindow popupWindow = new PopupWindow(contentView,
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-
-        popupWindow.setTouchable(true);
-
-        popupWindow.setTouchInterceptor(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-                // 这里如果返回true的话，touch事件将被拦截
-                // 拦截后 PopupWindow的onTouchEvent不被调用，这样点击外部区域无法dismiss
-            }
-        });
-
-        textCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                currentSet_year = year;
-                currentSet_month = month;
-                currentSet_day = day;
-                popupWindow.dismiss();
-            }
-        });
-
-        textConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                popupWindow.dismiss();
-                fragment_date.setText(currentSet_year + mContext.getResources().getString(R.string.text_year) + currentSet_month + mContext.getResources().getString(R.string.text_month));
-            }
-        });
-
-        popupWindow.showAtLocation(floating_calendar, Gravity.BOTTOM, 0, 0);
-
-    }
-
-    private boolean leapyear(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-            return true;
-        else
-            return false;
-    }
-
-    /**
-     * 自定义滚动框分隔线颜色
-     */
-    private void setNumberPickerDividerColor(NumberPicker number) {
-        Field[] pickerFields = NumberPicker.class.getDeclaredFields();
-        for (Field pf : pickerFields) {
-            if (pf.getName().equals("mSelectionDivider")) {
-                pf.setAccessible(true);
-                try {
-                    //设置分割线的颜色值
-                    pf.set(number, new ColorDrawable(ContextCompat.getColor(getActivity(), R.color.white)));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            }
-        }
-    }
 
     @Override
     public void onRefresh() {
         isRefresh = true;
-        selecteFloating();
+        selecteFloating(appUser.getId(), page, starttime, endtime);
     }
 
     @Override
     public void onPullUpRefresh() {
         isRefresh = false;
         if (page < totalPage) {
-            selecteFloating();
+            selecteFloating(appUser.getId(), page, starttime, endtime);
         } else {
             finishRefresh();
             showToast(getString(R.string.no_more));
@@ -362,4 +192,41 @@ public class FloatingRecordFragment extends BaseFragment implements View.OnClick
         refreshLayout.setRefreshing(false);
         refreshLayout.setPullUpRefreshing(false);
     }
+
+    public void showDatePicker() {
+        LWheelDialog dialog = new LWheelDialog(mContext, LWheelDialog.LWheelDialogType.DATE);
+        dialog.setConfirmListenter(this);
+        Window window = dialog.getWindow();
+        window.setGravity(Gravity.BOTTOM); // 此处可以设置dialog显示的位置
+        window.setWindowAnimations(R.style.dialogstyle_vertical); // 添加动画
+        dialog.setCancelable(true);
+        dialog.show();
+        Activity activity = (Activity) mContext;
+        WindowManager m = activity.getWindowManager();
+        Display d = m.getDefaultDisplay(); // 获取屏幕宽、高用
+        WindowManager.LayoutParams p = window.getAttributes(); // 获取对话框当前的参数值
+        p.height = (int) (d.getHeight() / 3);
+        p.width = (int) (d.getWidth());
+        dialog.onWindowAttributesChanged(p);
+        window.setAttributes(p);
+    }
+
+    /**
+     * 点击确定后获取到的时间
+     *
+     * @param year
+     * @param month
+     * @param day
+     */
+    @Override
+    public void onConfirmListener(String year, String month, String day) {
+        String month2 = month.length() == 2 ? month : "0" + month;
+        String day2 = day.length() == 2 ? day : "0" + day;
+        String s = year + "-" + month2 + "-" + day2;
+        starttime = s + " 00:00:00";
+        endtime = s + " 23:59:59";
+        isRefresh = true;
+        selecteFloating(appUser.getId(), page, starttime, endtime);
+    }
+
 }
